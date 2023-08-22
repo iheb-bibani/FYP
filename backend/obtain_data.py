f523e74a534106ef022f7999e16fdf9579da0470
vrsb99 <vigneshrsb99@gmail.com>
2023-08-22 16:03:13 +0800

    obtain data

---
+++ backend/obtain_data.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import yfinance as yf
+import datetime
+import sqlite3
+import time
+from urllib.error import HTTPError
+
+def main():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    ticker_source = cursor.execute('SELECT * FROM equities').fetchall()
+    cursor.close()
+    data = pd.DataFrame(ticker_source, columns=['id', 'name', 'ticker', 'sector'])
+    print(data)
+    #download_and_store_historical_data()
+    stock_data_dict = get_all_stock_data()
+    for ticker, data in stock_data_dict.items():
+        print(f"Data for {ticker}:")
+        print(data)
+        
+def download_and_store_historical_data():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    for idx, row in data.iterrows():
+        ticker = row['ticker']
+        print(f"Fetching historical data for {ticker}")
+
+        # Create a new table for each stock if it doesn't exist
+        table_name = f"stock_{ticker[:3]}" # Remove the .SI suffix
+        cursor.execute(f'CREATE TABLE IF NOT EXISTS {table_name} (Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume INTEGER, Dividends REAL, Stock_Splits REAL)')
+
+        # Get the latest date in the database
+        latest_date_in_db = cursor.execute(f'SELECT MAX(Date) FROM {table_name}').fetchone()[0]
+        if latest_date_in_db:
+            # Start from the day after the latest date in the database
+            start_date = (datetime.datetime.strptime(latest_date_in_db, '%Y-%m-%d') + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        else:
+            # Starts from 2010-01-01 if the table is empty
+            start_date = "2010-01-01"
+
+        # Handling Yahoo's rate limiter
+        while True:
+            try:
+                stock_data = yf.Ticker(ticker)
+                historical_data = stock_data.history(period="1d", start=start_date)
+                historical_data.fillna(0, inplace=True)
+                break
+            except HTTPError:
+                print(f"Rate limit exceeded for {ticker}. Retrying in 60 seconds...")
+                time.sleep(60)
+
+        # Insert new rows into the table
+        for index, row in historical_data.iterrows():
+            cursor.execute(f"INSERT INTO {table_name} VALUES ('{index.strftime('%Y-%m-%d')}', {row['Open']}, {row['High']}, {row['Low']}, {row['Close']}, {row['Volume']}, {row['Dividends']}, {row['Stock Splits']})")
+
+    connection.commit()
+    connection.close()
+    
+def get_all_stock_data():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    tickers = cursor.execute('SELECT ticker FROM equities').fetchall()
+
+    stock_data_dict = {}
+    for ticker_tuple in tickers:
+        ticker = ticker_tuple[0]
+        table_name = f"stock_{ticker[:3]}" # Remove the .SI suffix
+        query = f'SELECT * FROM {table_name}'
+        data = cursor.execute(query).fetchall()
+        columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock_Splits']
+        stock_data_dict[ticker] = pd.DataFrame(data, columns=columns)
+
+    cursor.close()
+    connection.close()
+    return stock_data_dict
+
+
+    
+if __name__ == "__main__":
+    main()
---
+++ backend/obtain_tickers.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import yfinance as yf
+import datetime
+import sqlite3
+
+def fetch_tickers():
+    return pd.read_html('https://topforeignstocks.com/listed-companies-lists/the-complete-list-of-listed-companies-in-singapore/')[0]
+
+def filter_stocks(ticker_source):
+    counter = 0
+    delisted = []
+    non_equity = []
+    insufficient_data = []
+    insufficient_market_cap = []
+
+    for idx, ticker in enumerate(ticker_source['Code']):
+        try:
+            data = yf.Ticker(ticker)
+            quote_type = data.info['quoteType']
+        except Exception as e:
+            print(f"{ticker} is delisted")
+            delisted.append(idx)
+            continue
+
+        if quote_type != 'EQUITY':
+            print(f"{ticker} is not an equity. Data not retrieved.")
+            non_equity.append(idx)
+            continue
+
+        if 'marketCap' not in data.info or 'averageVolume' not in data.info:
+            print(idx, ticker)
+            print(f"{ticker} has insufficient data. Data not retrieved.")
+            insufficient_data.append(idx)
+            continue
+
+        market_cap = data.info['marketCap']
+        avg_volume = data.info['averageVolume']
+        first_trade_date = datetime.datetime.fromtimestamp(data.info['firstTradeDateEpochUtc']).strftime('%Y-%m-%d')
+        if market_cap < 10e6 or avg_volume < 500e3 or first_trade_date > '2010-01-01':
+            print(f"{ticker} has insufficient market cap or average volume. Data not retrieved.")
+            insufficient_market_cap.append(idx)
+        else:
+            counter += 1
+
+    print(f"{counter} tickers have sufficient market cap and average volume.")
+    to_drop = delisted + non_equity + insufficient_data + insufficient_market_cap
+    ticker_source.drop(to_drop, inplace=True)
+    return ticker_source
+
+def store_to_db(ticker_source):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS equities (id INTEGER PRIMARY KEY, name TEXT, ticker TEXT, sector TEXT)')
+    data = cursor.execute('SELECT * FROM equities').fetchall()
+
+    if data == []:
+        for idx,stock in enumerate(ticker_source.iterrows()):
+            cursor.execute(f'INSERT INTO equities VALUES ({idx}, "{stock[1][1]}", "{stock[1][2]}", "{stock[1][3]}")')
+    else:
+        data = pd.DataFrame(data, columns=['id', 'name', 'ticker', 'sector'])
+        print(data)
+        
+    connection.commit()
+    connection.close()
+
+if __name__ == '__main__':
+    ticker_source = fetch_tickers()
+    print(ticker_source)
+    filtered_stocks = filter_stocks(ticker_source)
+    print(f'{len(filtered_stocks)} stocks remaining')
+    store_to_db(filtered_stocks)
